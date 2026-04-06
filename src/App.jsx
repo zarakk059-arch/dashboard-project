c3094b08e04e043c9067ad00764d8a0f8849bc10
zarak <zarakk059@gmail.com>
2026-04-06 10:05:36 +0500

    Initial commit with Sidebar and App updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -9,26 +9,32 @@ import Finance from "./components/Finance";
 import Project from "./components/Project";
 
 const App = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <BrowserRouter>
       <div className="flex">
+        
         {/* Sidebar */}
-        <Sidebar />
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
-        {/* Main content */}
         <div className="flex-1">
-          <Navbar />
+          
+          {/* Navbar */}
+          <Navbar setSidebarOpen={setSidebarOpen} />
+
+          {/* Routes */}
           <Routes>
             {/* Default route */}
             <Route path="/" element={<Ecommerce />} />
 
-            {/* Other pages */}
-             <Route path="/ecommerce" element={<Ecommerce />} />
+            <Route path="/ecommerce" element={<Ecommerce />} />
             <Route path="/analytics" element={<Analytics />} />
             <Route path="/crm" element={<Crm />} />
             <Route path="/finance" element={<Finance />} />
             <Route path="/projects" element={<Project />} />
           </Routes>
+
         </div>
       </div>
     </BrowserRouter>
