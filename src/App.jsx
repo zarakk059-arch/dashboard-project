4df6f3d936593166b45b55a01150dcd00cdb38a6
zarak <zarakk059@gmail.com>
2026-04-06 09:56:47 +0500

    Set Ecommerce as default, update Sidebar with NavLink and active highlighting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
@@ -9,29 +9,26 @@ import Finance from "./components/Finance";
 import Project from "./components/Project";
 
 const App = () => {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <BrowserRouter>
       <div className="flex">
-        
         {/* Sidebar */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <Sidebar />
 
+        {/* Main content */}
         <div className="flex-1">
-          
-          {/* Navbar */}
-          <Navbar setSidebarOpen={setSidebarOpen} />
-
-          {/* Routes */}
+          <Navbar />
           <Routes>
-            <Route path="/ecommerce" element={<Ecommerce />} />
+            {/* Default route */}
+            <Route path="/" element={<Ecommerce />} />
+
+            {/* Other pages */}
+             <Route path="/ecommerce" element={<Ecommerce />} />
             <Route path="/analytics" element={<Analytics />} />
             <Route path="/crm" element={<Crm />} />
             <Route path="/finance" element={<Finance />} />
             <Route path="/projects" element={<Project />} />
           </Routes>
-
         </div>
       </div>
     </BrowserRouter>
